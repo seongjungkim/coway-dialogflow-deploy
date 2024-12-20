0d5bf75cc179dd3de15b9ef8de5608c1c5597b29
SeongJung Kim <sjkim@tpcg.co.kr>
2024-12-20 17:47:55 +0900

    Dialogflow Export 추가중

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 Coway Dialogflow CX Deployment (Github) by TypeScript
 
 
+##
+
+```bash
+npm install @google-cloud/dialogflow-cx
+```
+
 ## Local Build & Test
 
 ```bash
@@ -14,4 +20,19 @@ tsc
 
 ```bash
 yarn test
+```
+
+```bash
+(node:27298) [MODULE_TYPELESS_PACKAGE_JSON] Warning: Module type of file:///Users/seongjungkim/Development/python/datacollector/coway/coway-dialogflow-deploy/dist/dialogflow.js is not specified and it doesn't parse as CommonJS.
+Reparsing as ES module because module syntax was detected. This incurs a performance overhead.
+To eliminate this warning, add "type": "module" to /Users/seongjungkim/Development/python/datacollector/coway/coway-dialogflow-deploy/package.json.
+(Use `node --trace-warnings ...` to show where the warning was created)
+(node:27298) [DEP0040] DeprecationWarning: The `punycode` module is deprecated. Please use a userland alternative instead.
+node:internal/process/promises:394
+    triggerUncaughtException(err, true /* fromPromise */);
+    ^
+```
+
+```bash
+Error: 7 PERMISSION_DENIED: Dialogflow API has not been used in project dl-cloud-search before or it is disabled. Enable it by visiting https://console.developers.google.com/apis/api/dialogflow.googleapis.com/overview?project=dl-cloud-search then retry. If you enabled this API recently, wait a few minutes for the action to propagate to our systems and retry.
 ```
--- src/dialogflow.ts
+++ src/dialogflow.ts
@@ -1,4 +1,4 @@
-import { SessionsClient } from "@google-cloud/dialogflow-cx";
+import { AgentsClient, SessionsClient } from "@google-cloud/dialogflow-cx";
 
 // 프로젝트 ID, 위치 ID, 에이전트 ID를 Dialogflow CX 콘솔에서 확인합니다.
 const projectId = "qnacom-service";
@@ -12,40 +12,115 @@ const agentId = "86920b8a-744d-4e24-8b2d-6d9a6ba8aac6";
 // 서비스 계정 키 파일 경로를 지정합니다.
 const credentialsPath = "/Users/seongjungkim/Development/python/datacollector/credentials/qnacom-service-94f96c83a4e3.json";
 
-// SessionsClient를 초기화합니다.
-//const client = new SessionsClient({ credentials: require(credentialsPath) });
-const client = new SessionsClient();
+export const detectIntent = async (text: string): Promise<void> => {
+    // SessionsClient를 초기화합니다.
+    //const client = new SessionsClient({ credentials: require(credentialsPath) });
+    const client = new SessionsClient();
 
-// 세션 경로를 구성합니다.
-const sessionId = Math.random().toString(36).substring(7);
-const sessionPath = client.projectLocationAgentSessionPath(
-  projectId,
-  locationId,
-  agentId,
-  sessionId
-);
+    // 세션 경로를 구성합니다.
+    const sessionId = Math.random().toString(36).substring(7);
+    const sessionPath = client.projectLocationAgentSessionPath(
+        projectId,
+        locationId,
+        agentId,
+        sessionId
+    );
 
-// 텍스트 쿼리를 생성합니다.
-const request = {
-  session: sessionPath,
-  queryInput: {
-    text: {
-      text: "안녕하세요",
-    },
-    languageCode: "ko",
-  },
-};
+    // 텍스트 쿼리를 생성합니다.
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: "안녕하세요",
+            },
+            languageCode: "ko",
+        },
+    };
 
-// DetectIntent 메서드를 사용하여 쿼리를 보냅니다.
-const [response] = await client.detectIntent(request);
+    // DetectIntent 메서드를 사용하여 쿼리를 보냅니다.
+    const [response] = await client.detectIntent(request);
 
-/*
-src/dialoglfow.ts:39:20 - error TS1378: Top-level 'await' expressions are only allowed 
-when the 'module' option is set to 'es2022', 'esnext', 'system', 'node16', 'nodenext', or 'preserve', and the 'target' option is set to 'es2017' or higher.
-39 const [response] = await client.detectIntent(request);
-*/
+    /*
+    src/dialoglfow.ts:39:20 - error TS1378: Top-level 'await' expressions are only allowed 
+    when the 'module' option is set to 'es2022', 'esnext', 'system', 'node16', 'nodenext', or 'preserve', and the 'target' option is set to 'es2017' or higher.
+    39 const [response] = await client.detectIntent(request);
+    */
 
-// 응답에서 텍스트를 추출합니다.
-const responseText = response.queryResult?.responseMessages?.[0]?.text?.text?.[0];
+    // 응답에서 텍스트를 추출합니다.
+    const responseText = response.queryResult?.responseMessages?.[0]?.text?.text?.[0];
 
-console.log(`Dialogflow CX 응답: ${responseText}`);
+    console.log(`Dialogflow CX 응답: ${responseText}`);
+}
+
+export const restoreAgent = async (name: string, gitSource: string): Promise<void> => {
+    // https://cloud.google.com/nodejs/docs/reference/dialogflow-cx/latest#using-the-client-library
+    // Dialogflow CX API: Node.js Client
+    // https://github.com/googleapis/google-cloud-node/blob/main/packages/google-cloud-dialogflow-cx/samples/generated/v3/agents.restore_agent.js
+
+    // Instantiates a client
+    const client = new AgentsClient();
+
+    async function callRestoreAgent() {
+        // Construct request
+        const request = {
+            name,
+            gitSource: {
+                trackingBranch: gitSource
+            }
+        };
+
+        // Run request
+        const [operation] = await client.restoreAgent(request);
+        const [response] = await operation.promise();
+        console.log(response);
+    }
+
+    callRestoreAgent();
+}
+
+export const exportAgent = async (name: string, message: string, gitDestination: string): Promise<void> => {
+  // https://cloud.google.com/nodejs/docs/reference/dialogflow-cx/latest#using-the-client-library
+  // https://cloud.google.com/nodejs/docs/reference/dialogflow-cx/latest/dialogflow-cx/protos.google.cloud.dialogflow.cx.v3.exportagentrequest-class
+  // https://cloud.google.com/nodejs/docs/reference/dialogflow-cx/latest/dialogflow-cx/protos.google.cloud.dialogflow.cx.v3.iexportagentrequest
+  // Dialogflow CX API: Node.js Client
+  // https://github.com/googleapis/google-cloud-node/blob/main/packages/google-cloud-dialogflow-cx/samples/generated/v3/agents.restore_agent.js
+
+  // Instantiates a client
+  const client = new AgentsClient();
+
+  async function callExportAgent() {
+      // Construct request
+      const request = {
+          dataFormat: 'JSON_PACKAGE',
+          name: name,
+          gitDestination: {
+              commitMessage: message,
+              trackingBranch: gitDestination
+          },
+          includeBigqueryExportSettings: false
+      };
+
+      /*
+      { 
+        dataFormat: string; 
+        name: string; 
+        gitDestination: { commitMessage: string; trackingBranch: string; }; 
+        includeBigqueryExportSettings: boolean; 
+      }
+      */
+
+      // Run request
+      const [operation] = await client.exportAgent(request);
+      const [response] = await operation.promise();
+      console.log(response);
+  }
+
+  callExportAgent();
+}
+
+//detectIntent('Hello');
+const name = `projects/${projectId}/locations/${locationId}/agents/${agentId}`;
+const branch = "main";
+const message = "Node.js 테스트";
+//restoreAgent(name, branch);
+exportAgent(name, message, branch);
